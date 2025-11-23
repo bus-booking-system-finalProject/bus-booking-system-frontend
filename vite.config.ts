9ded9a647150210fd53e1b98e3221734d91cfe5c
thienanle04 <lethienan1106@gmail.com>
2025-11-23 18:56:35 +0700

    update: frontend service docker-compose, vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    host: true
+    host: true,
   },
-  base: '/IA3-Web-Frontend/',
-})
+  base: '/bus-booking-system-frontend/',
+});
